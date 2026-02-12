2b0d97bae4e1085c16a70fea5f6c31459551e22e
cassildobez2-star <cassildobez2@gmail.com>
2026-02-12 09:50:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-let currentMode = "basica";
-
-function setMode(mode) {
-    currentMode = mode;
-}
-
 function calculate() {
     const expr = document.getElementById("expression").value;
     const output = document.getElementById("output");
 
-    try {
-
-        if (currentMode === "calculo") {
-            const derivada = math.derivative(expr, 'x').toString();
-            output.innerText = derivada;
-        }
-
-        else {
-            const resultado = math.evaluate(expr);
-            output.innerText = resultado;
-        }
-
-    } catch (error) {
-        output.innerText = "Erro: expressão inválida";
-        console.error(error);
-    }
+    output.innerText = "Você digitou: " + expr;
 }
